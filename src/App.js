74e609a1dc25a8b1ad4bf62ad2ed1df8353be18f
vijayst <vijayst@gmail.com>
2019-03-02 12:42:28 +0530

    header layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import styles from './app.module.css';
+import { Menu } from 'semantic-ui-react';
 
 class App extends Component {
     render() {
         return (
             <div className={styles.app}>
-                <h1 className="ui header">Hello world</h1>
+                <header>
+                    <div className={styles.brand}>Pollz</div>
+                    <Menu secondary>
+                        <Menu.Item name="polls" content="My Polls" />
+                        <Menu.Item name="logout" content="Logout" />
+                        <Menu.Item name="create" content="Create Poll" />
+                    </Menu>
+                </header>
             </div>
         );
     }
