db777d125a2b1ad1e6b63bde0345a3b93425dbef
vijayst <vijayst@gmail.com>
2019-03-04 18:36:51 +0530

    some content for the home page

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import firebase from '../util/firebase';
+import { Segment, Header, Button } from 'semantic-ui-react';
+import UserContext from '../auth/UserContext';
+import styles from './home.module.css';
+import { Link } from 'react-router-dom';
 
-export default function Home() {
+export default function MyPolls() {
+    let [polls, setPolls] = useState([]);
+    const user = useContext(UserContext);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection('polls')
+            .get()
+            .then(snapshot => {
+                const polls = [];
+                snapshot.forEach(doc => {
+                    polls.push(doc.data());
+                });
+                setPolls(polls);
+            });
+    }, []);
+
     return (
-        <div>Hello world</div>
+        <div className={styles.page}>
+            <h1>Open Polls</h1>
+            {polls.length === 0 && (
+                <Segment placeholder>
+                    <Header icon>There are no open polls!!!</Header>
+                </Segment>
+            )}
+            {polls.length && !user ? (
+                <Segment placeholder>
+                    <Header icon>Login to poll</Header>
+                    <Button primary as={Link} to="/login">
+                        Login
+                    </Button>
+                </Segment>
+            ) : null}
+            {polls.map(poll => (
+                <Segment style={{ marginBottom: 32 }} key={poll.uid}>
+                    <div className={styles.question}>{poll.question}</div>
+                    <ol className={styles.list}>
+                        {poll.options.map((option, index) => (
+                            <li key={index}>{option.text}</li>
+                        ))}
+                    </ol>
+                </Segment>
+            ))}
+        </div>
     );
 }
